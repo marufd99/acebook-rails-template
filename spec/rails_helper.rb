8b59df55b2327455062d267679d1696f181bd3ba
Anna Cotugno <annaeuridice@gmail.com>
2019-12-13 15:02:29 +0000

    removed require pg form helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'simplecov'
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'pg'
+
 
 SimpleCov.start
 # Requires supporting ruby files with custom matchers and macros, etc, in
