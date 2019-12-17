97918a1b42796035b67fee687b4f45f2e69f87f7
Anna Cotugno <annaeuridice@gmail.com>
2019-12-17 11:02:19 +0000

    addded add_secure_password migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191213165443) do
+ActiveRecord::Schema.define(version: 20191217104252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20191213165443) do
     t.string "name"
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
