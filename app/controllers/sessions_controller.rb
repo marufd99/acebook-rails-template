988c2919c31866819ad678205e86fb38901f3485
Student <maruf.d99@gmail.com>
2019-12-19 17:15:01 +0000

    Fixed redirect link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
   def logout
     session[:user_id] = nil
     flash[:notice] = "Logged out"
-    redirect_to sessions_login_url
+    redirect_to root_path
   end
 
   def welcome
